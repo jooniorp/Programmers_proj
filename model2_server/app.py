1852320a301c78afe9d1a4523b476013983e5dfa
jooniorp <129948093+jooniorp@users.noreply.github.com>
2024-01-17 14:47:46 +0900

    This is Docker Environment for model2 server
    Model2 Server Docker Container

--- model2_server/app.py
+++ model2_server/app.py
@@ -1,6 +1,4 @@
-from flask import Flask, request, render_template_string, jsonify
 import openai
-from pymongo import MongoClient
 import gradio as gr
 
 # OpenAI API 키 설정
@@ -27,24 +25,4 @@ iface = gr.Interface(
 # Gradio 웹 애플리케이션 실행
 if __name__ == '__main__':
     # Gradio 웹 애플리케이션 실행
-  iface.launch(share=True,server_name="0.0.0.0", server_port=2000)  # share=True로 설정하면 외부에서도 접근 가능한 링크 생성
-'''
-# MongoDB 연결 설정
-client = MongoClient('mongodb:27017')  # 'mongodb'는 MongoDB 컨테이너의 이름
-db = client['testdb']  # 데이터베이스 이름
-collection = db['testcollection']  # 컬렉션 이름
-
-# 텍스트 데이터를 받아 MongoDB에 저장
-@app.route('/save_to_mongo', methods=['POST'])
-def save_to_mongo():
-    data = request.get_json()
-    text_data = data.get('text_data', '')
-
-     # MongoDB에 데이터 저장
-    result = collection.insert_one({'text_data': text_data})
-
-    return jsonify({'result': 'Data saved to MongoDB!', 'mongo_id': str(result.inserted_id)})
-
-'''
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+  iface.launch(share=False,server_name="0.0.0.0", server_port=2000)  # share=True로 설정하면 외부에서도 접근 가능한 링크 생성
