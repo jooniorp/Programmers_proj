0ad95c5ed608b28997932eab1f82f27719ab0113
jhs36 <133088174+jhs36@users.noreply.github.com>
2024-01-18 18:24:07 +0900

    Update save_to_mongodb.py

--- save_to_mongodb.py
+++ save_to_mongodb.py
@@ -16,14 +16,3 @@ def save_to_mongodb(my_dict):
 
     # 결과 확인
     print(f"Inserted document ID: {result.inserted_id}")
-"""
-이제 각 파일은 명령줄에서 실행할 때 텍스트 파일의 경로를 매개변수로 전달받습니다.
-
-예를 들어:
-in bash)
-
-python read_and_create_dict.py your_text_file.txt
-python save_to_mongodb.py your_text_file.txt
-
-이렇게 하면 다른 이름의 텍스트 파일을 사용하여 각각의 스크립트를 실행할 수 있습니다.
-"""
